4403348b51174e91a5ade232de932dd1ca5bd168
ducusinabdul <ducusinabdul@gmail.com>
2024-05-11 11:10:05 +0800

    HomeWork 3 Initial Push

---
+++ src/main/java/portal/gradecalculator/GradeCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package portal.gradecalculator;
+
+/**
+ *
+ * @author ducus
+ */
+public class GradeCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
